6cc21d38cac60ced408442cf8abbc9701e6855cf
indranil.sengupta <indranil.sengupta@gmail.com>
2013-01-02 12:06:02 +0530

    checking in a nop test to check CI

--- tests/src/com/example/accion/HelloWorldActivityTest.java
+++ tests/src/com/example/accion/HelloWorldActivityTest.java
@@ -24,9 +24,9 @@ public class HelloWorldActivityTest extends ActivityInstrumentationTestCase2<Hel
         assertTrue("first test says true", true);
     }
 
-    public void testToCheckCI() {
-        assertTrue("first test says true", true);
-    }
+//    public void testToCheckCI() {
+//        assertTrue("first test says true", true);
+//    }
 
     public void testWelcomeMessageIsOk() {
         HelloWorldActivity act = getActivity();
